922b72f65ee181cf5ca0699e0ea43f828dfd4d9d
BrianKendalBucek <bbucek@gmail.com>
2022-08-10 01:37:43 +0000

    screenshots added

--- constants.js
+++ constants.js
@@ -1,7 +1,20 @@
 const IP = 'localhost';
 const PORT = 50541;
+const MOVE_UP_KEY = 'w';
+const MOVE_DOWN_KEY = 's';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_RIGHT_KEY = 'd';
+const MESSAGE_1 = 'r';
+const MESSAGE_2 = 'f';
+
 
 module.exports = {
   IP,
   PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGE_1,
+  MESSAGE_2,
 };
--- input.js
+++ input.js
@@ -1,3 +1,10 @@
+const { MOVE_UP_KEY } = require("./constants");
+const { MOVE_DOWN_KEY } = require("./constants");
+const { MOVE_LEFT_KEY } = require("./constants");
+const { MOVE_RIGHT_KEY } = require("./constants");
+const { MESSAGE_1 } = require("./constants");
+const { MESSAGE_2 } = require("./constants");
+
 let connection;
 
 const setupInput = function (conn) {
@@ -16,29 +23,29 @@ const handleUserInput = function(key) {
     process.exit();
   }
   
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
 
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
 
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
   
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
 
-  if (key === 'f') {
+  if (key === MESSAGE_1) {
     connection.write("Say: Gimmie food");
   }
-  if (key === 'r') {
+  if (key === MESSAGE_2) {
     connection.write("Say: So hungry");
   }
 };
 
 
-module.exports = { setupInput } ;
+module.exports = { setupInput, connection } ;
